614d13213182e5845c92a8dad36beee143161130
DekaR0n <margaritach_vl@mail.ru>
2022-05-16 19:39:43 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-function generateNew() {
 
+function generateNew() {
+  let quotes= [
+  {author: "Depp", quote: "gfd"},
+  {author: "A", quote: "rgre"},
+  {author: "B", quote: "gfd"},
+  {author: "C", quote: ""},
+  {author: "D", quote: ""},
+  {author: "", quote: "rwqr"},
+  {author: "", quote: ""},
+  {author: "G", quote: "fwfr"},
+  {author: "F", quote: "regre"},
+  {author: "T", quote: ""},
+  {author: "K", quote: ""},
+  {author: "J", quote: ""}
+];
+  let quoteRandom = quotes[Math.round(Math.random() * (13))];
+  document.getElementById("author").innerText = quoteRandom.author;
+  document.getElementById("gen-def").innerText = quoteRandom.quote;
+  return;
 }
---
+++ js/quotes.js
@@ -0,0 +1,15 @@
+let quotes= [
+  {author: "Depp", quote: "jhg"},
+  {author: "A", quote: "jhgfd"},
+  {author: "B", quote: "ddhh"},
+  {author: "C", quote: "hhhh"},
+  {author: "D", quote: "hki"},
+  {author: "", quote: ""},
+  {author: "", quote: ""},
+  {author: "G", quote: ""},
+  {author: "F", quote: ""},
+  {author: "T", quote: ""},
+  {author: "K", quote: ""},
+  {author: "J", quote: ""}
+];
+export default quotes;
